68b570d2e7284e84f5f1161cd450e3012e6861cb
kmilo3012-del <68394825+kmilo3012-del@users.noreply.github.com>
2024-08-26 17:40:15 -0500

    Create S4(C3(P10)).sql

---
+++ Capitulo 3/S4(C3(P10)).sql
@@ -0,0 +1,9 @@
+/*
+	PREGUNTAS: 
+
+	1. ¿Cual es el promedio de los las calificaciones de los usuarios para las canciones con id mayor a 5? 
+	2. ¿Cuantos usuarios hay regritados con fecha de registro posterior al inicio de los 2000´s?
+	3. ¿Cuales son las canciones que tienen la mayor y menor duracion en cada idioma?
+	4. ¿ Cual es la suma de los usuarios de cada pais?
+	5. ¿Cual es el año de mas reproducciones para cada cancion?
+*/
